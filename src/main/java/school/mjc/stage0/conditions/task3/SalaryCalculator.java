d83c6c41303bda2793e75ddffda956dd245f5cee
SarvarKhalimov <khalimovsarvar@gmail.com>
2022-09-18 07:05:01 +0500

    Solve task #3 with if statements

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary <= 10000) {
+            System.out.println(salary * 0.85);
+        } else if (10000 < salary && salary <= 20000) {
+            System.out.println(salary * 0.92);
+        } else if (salary > 20000) {
+            System.out.println(salary * 0.8);
+        } else if (salary < 0) {
+            System.out.println("wrong input!");
+        }
     }
 }
